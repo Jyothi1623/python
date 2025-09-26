5b6d038abaf504a7f670233dc0d3d6f19569aa18
Jyothi1623 <jyothibsravana@gmail.com>
2025-09-26 19:11:36 +0530

    vowels

---
+++ vowelsc.py
@@ -0,0 +1,42 @@
+# name=input('enter :')
+# vowels='aeiouAEIOU'
+# # count=0
+# # count1=0
+# for char in name:
+#     if char in vowels:
+#         print(char)
+#         # count+=1
+# else:
+#     if char not in vowels:
+#             print(char)
+#             # count1+=1
+
+# print('count',count)
+# print(count1)
+
+
+
+
+#consonants
+name=input('enter :')
+vowels='aeiouAEIOU'
+# count=0
+# count1=0
+for char in name:
+    if char in vowels:
+        print(char)
+        # count+=1
+    if char not in vowels:
+            print(char)
+            # count1+=1
+
+# # print('count',count)
+# print(count1)
+
+#to ptint first vowel
+# n=input('enter a name:')
+# vowels='aeiouAEIOU'
+# for char in n:
+#       if char in vowels:
+#             print(char)
+#             break
